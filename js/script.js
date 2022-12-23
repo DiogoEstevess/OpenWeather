f06ac43fb70d244bc41f291017cd0b493cbfb726
Diogo Esteves <diogomcesteves@gmail.com>
2022-12-23 16:58:40 +0000

    alterar o codigo posteriormente

--- js/script.js
+++ js/script.js
@@ -3,11 +3,16 @@ let weather = {
 
     fetchWeather: function (city) {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" +
-          this.apiKey + "&lang=pt_br"
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey + "&lang=pt_br"
       )
-        .then((response) => response.json)
-        .then((data) => console.log(data));
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
     },
 
     displayWeather: function(data) {
@@ -15,15 +20,15 @@ let weather = {
     //   const { icon, description } = data.weather[0];
     //   const { temp, humidity } = data.main;
     //   const { speed } = data.wind;
-    //   document.querySelector(".city").innerText = "Weather in " + name;
+    //   document.querySelector(".city").innerText = "Tempo em " + name;
     //   document.querySelector(".icon").src =
     //     "https://openweathermap.org/img/wn/" + icon + ".png";
     //   document.querySelector(".description").innerText = description;
     //   document.querySelector(".temp").innerText = temp + "°C";
     //   document.querySelector(".humidity").innerText =
-    //     "Humidity: " + humidity + "%";
+    //     "Humidade: " + humidity + "%";
     //   document.querySelector(".wind").innerText =
-    //     "Wind speed: " + speed + " km/h";
+    //     "Vento: " + speed + " km/h";
     //   document.querySelector(".weather").classList.remove("loading");
     //   document.body.style.backgroundImage =
     //     "url('https://source.unsplash.com/1600x900/?" + name + "')";
