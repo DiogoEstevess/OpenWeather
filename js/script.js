cca9998e984dc0c6bf7fa7e3a6e31d6b37dc95a8
Diogo Esteves <diogomcesteves@gmail.com>
2022-12-29 16:21:54 +0000

    k

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let weather = {
 
     fetchWeather: function (city) {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey + "&lang=pt_br"
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey + "&lang=pt"
       )
       .then((response) => {
         if (!response.ok) {
