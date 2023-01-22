66280756c8d0a3f8e1735390e8e2b8c4f2896634
2220879 <2220879@my.ipleiria.pt>
2023-01-22 15:34:05 +0000

    Problema textbox_Pesquisa

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,46 +1,81 @@
-let weather = {
-  apiKey: "74d90f8a0dc103698bf1cb634e5dc227",
+/*var button = document.getElementById("#button_search");
+    button.onclick = function() {
+      var cidade = $('#barra_pesquisa').val();
+      let weather = {
+        apiKey: "74d90f8a0dc103698bf1cb634e5dc227",
+      
+        fetchWeather: function (city) {
+          fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric&appid=" + this.apiKey + "&lang=pt"
+          )
+            .then((response) => {
+              if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found.");
+              }
+              return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+        },
+      
+        displayWeather: function (data) {
+          const { name } = data;
+          const { icon, description } = data.weather[0];
+          const { temp, humidity } = data.main;
+          const { speed } = data.wind;
+          document.querySelector(".cidade").innerText = name;
+          document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+          document.querySelector(".descricao").innerText = description;
+          document.querySelector(".temperatura").innerText = temp + "°C";
+          document.querySelector(".humidade").innerText = "Humidade: " + humidity + "%";
+          document.querySelector(".vento").innerText = "Vento: " + speed + " km/h";
+        },
+      };
+      weather.fetchWeather(cidade);
+    }*/
 
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey + "&lang=pt"
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
 
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".cidade").innerText = name;
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".descricao").innerText = description;
-    document.querySelector(".temperatura").innerText = temp + "°C";
-    document.querySelector(".humidade").innerText = "Humidade: " + humidity + "%";
-    document.querySelector(".vento").innerText = "Vento: " + speed + " km/h";
-  },
-
-  search: function () {
-    this.fetchWeather(document.querySelector("#pesquisa").value);
-  },
+function Obter_Cidade() {
+  alert("Estou aqui..........");
+  let cidade = document.getElementById('#barra_pesquisa'); /*$('barra_pesquisa').val();*/
+  alert("A cidade inserida foi: " + cidade);
+  let weather = {
+    apiKey: "74d90f8a0dc103698bf1cb634e5dc227",
+  
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric&appid=" + this.apiKey + "&lang=pt"
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("...Cidade não encontrada...");
+            throw new Error("...Cidade não encontrada...");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".cidade").innerText = name;
+      document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".descricao").innerText = description;
+      document.querySelector(".temperatura").innerText = temp + "°C";
+      document.querySelector(".humidade").innerText = "Humidade: " + humidity + "%";
+      document.querySelector(".vento").innerText = "Vento: " + speed + " km/h";
+    },
+  };
+  weather.fetchWeather(cidade);
 };
 
-// document.querySelector(".flex button").addEventListener("click", function () {
-//   weather.search();
-// });
-
-// document.querySelector(".form-control me-2 pesquisa").addEventListener("keyup", function (event) {
-//   if (event.key == "Enter") {
-//     weather.search();
-//   }
-// });
-
-weather.fetchWeather("Torres Vedras");
+/*document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });*/
